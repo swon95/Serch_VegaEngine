9a3cfdb7668551eeaa6305a37fab42c40dd51b73
swon95 <shimtkddnjs13@gmail.com>
2023-11-21 01:12:26 +0900

    feat: infinite loop

---
+++ index.js
@@ -0,0 +1,60 @@
+// parsing dependencies
+const cheerio = require("cheerio");
+const axios = require("axios");
+
+// fetch-url
+const baseURL = "https://en.wikipedia.org";
+
+let DBList = {
+    // url == key
+    "https://en.wikipedia.org/wiki/Computer": {
+        title: "Computer",
+        keywords: ["computer", "hardware", "software"],
+        score: 1, // Increments by 1 each it reference.
+    },
+};
+
+const crawl = async (url) => {
+    console.log("fetch URL: ", url);
+
+    const htmlDocs = await axios.get(url);
+    // $ == jQuery convention
+    const $ = cheerio.load(htmlDocs.data);
+    const links = $("a"); // a tag parsing
+    const title = $("h1").text();
+
+    if (DBList[url]) {
+        DBList[url].score += 1;
+    } else {
+        DBList[url] = {
+            title,
+            score: 1,
+        };
+    }
+
+    links.each((index, element) => {
+        const href = $(element).attr("href");
+
+        // exception handling
+        if (!href) return;
+
+        // 만약, href 가 http:// 또는 https:// 로 시작한다면 새로운 URL 로 인식하여
+        // crawl 함수 호출
+        if (href.startsWith("http://") || href.startsWith("https://")) {
+            crawl(href);
+            return;
+        }
+
+        // 그렇지 않은 경우 현재 URL 의 경로를 기준으로 새로운 URL 을 만들어줘야함
+        // ex) 현재 URL 이 https://en.wikipedia.org/wiki/Computer 이고,
+        // href 가 /wiki/Computer_science 라면,
+        // https://en.wikipedia.org/wiki/Computer_science 로 만들어줘야함
+
+        // const originURL = url.slice(0, url.lastIndexOf('/'))
+        const originURL = new URL(url).origin;
+        const newURL = originURL + href;
+        crawl(newURL);
+    });
+};
+
+crawl(baseURL);
